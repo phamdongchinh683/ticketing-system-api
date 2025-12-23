e1ae2845e51edebb8e69ed0887e6fc92e69089d1
pham dong chinh <dchinh6803@gmail.com>
2025-12-23 17:06:54 +0700

    test cicd

--- src/database/types.ts
+++
@@ -1,8 +0,0 @@
-import type { Database as KyselyDatabase } from 'kysely'
-import type { AuthTable } from './auth/table.js'
-
-// Main database type definition
-export interface Database extends KyselyDatabase {
-    auth: AuthTable
-}
-
