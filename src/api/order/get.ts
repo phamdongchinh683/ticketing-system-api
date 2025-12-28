a09d208d5f823b52f3a45aaced1588a2c0ebb975
pham dong chinh <dchinh6803@gmail.com>
2025-12-28 13:02:58 +0700

    example api test

---
+++ src/api/order/get.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+import { api, bearer, endpoint, tags } from '../../app/api.js'
+
+const __filename = new URL('', import.meta.url).pathname
+
+api.route({
+    ...endpoint(__filename),
+    handler: async request => {
+        return {
+            message: 'OK',
+        }
+    },
+
+    schema: {
+        querystring: z.object({
+            id: z.string(),
+        }),
+        response: { 200: z.object({ message: z.string() }) },
+        tags: tags(__filename),
+    },
+})
